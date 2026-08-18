2f93be2c5bc8b3f92a7ebeec9e7e7f1ca0414d51
Mason Prey <mprey@users.noreply.github.com>
2026-08-18 16:50:11 -0400

    fix: bump orchestrator

--- cdk/lib/config.ts
+++ cdk/lib/config.ts
@@ -11,7 +11,7 @@ export interface MarketDataConfig {
 }
 
 const defaultConfig = {
-  collectorOrchestratorVersion: "1.5.1",
+  collectorOrchestratorVersion: "1.5.2",
 }
 
 const REGISTRY_API_KEY_IDS: { [stage in Stage]?: string } = {
